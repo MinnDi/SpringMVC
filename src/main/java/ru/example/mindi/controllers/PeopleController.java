a0e45e5ca2b16c8eb0da750c0ab7e2cb5878c010
MinnDi <dlfminn675@gmail.com>
2022-11-16 00:38:56 +0300

    CRUD

--- src/main/java/ru/example/mindi/controllers/PeopleController.java
+++ src/main/java/ru/example/mindi/controllers/PeopleController.java
@@ -46,7 +46,7 @@ public class PeopleController {
         return "people/edit";
     }
 
-    @PostMapping("/{id}")
+    @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
         personDao.update(id, person);
         return "redirect:/people";
